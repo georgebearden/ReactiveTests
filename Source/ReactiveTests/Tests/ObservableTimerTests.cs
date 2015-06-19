425bde8ff06d793d424fb7af0b1c1e3b8acfc1d6
georgebearden <george@goerden.com>
2015-06-19 11:22:12 -0400

    Created an example ObservableTimer.

---
+++ Source/ReactiveTests/Tests/ObservableTimerTests.cs
@@ -0,0 +1,49 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+using Xunit;
+
+namespace ReactiveTests.Tests
+{
+  public class ObservableTimerTests
+  {
+    public IObservable<DateTime> CreateObservableTimer()
+    {
+      return Observable.Create<DateTime>(observer =>
+        {
+          ElapsedEventHandler elapsed = (s, e) => observer.OnNext(e.SignalTime);
+
+          var timer = new System.Timers.Timer(1000);
+          timer.Elapsed += elapsed;
+          timer.Start();
+
+          return Disposable.Create(() =>
+          {
+            timer.Elapsed -= elapsed;
+            timer.Dispose();
+          });
+        });
+    }
+
+    [Fact]
+    public void ObservableTimerCallsOnNext()
+    {
+      var observer = new Mock<IObserver<DateTime>>();
+
+      var timer = CreateObservableTimer();
+      using (var disposable = timer.Subscribe(observer.Object))
+      {
+        Thread.Sleep(2000);
+      }
+
+      observer.Verify(obs => obs.OnNext(It.IsAny<DateTime>()), Times.AtLeastOnce());
+    }
+  }
+}
