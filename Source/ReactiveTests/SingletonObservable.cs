c13c8b70462ecead1bfa3c4e24b2b7049009b617
georgebearden <george@goerden.com>
2015-09-11 16:26:12 -0400

    Working on a reusable singleton observable model.

---
+++ Source/ReactiveTests/SingletonObservable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReactiveTests
+{
+  public class SingletonObservable<T> : IObservable<T>, IDisposable
+  {
+    private readonly Func<IObserver<T>, IDisposable> _impl;
+    private readonly CancellationTokenSource _cancelToken;
+    private readonly Lazy<Subject<T>> _subject;
+
+    public SingletonObservable( Func<IObserver<T>, IDisposable> impl )
+    {
+      _impl = impl;
+      _cancelToken = new CancellationTokenSource();
+
+      _subject = new Lazy<Subject<T>>( () =>
+      {
+        var subject = new Subject<T>();
+        //_impl();
+        return subject;
+      } );
+    }
+
+    public void Dispose()
+    {
+      if ( !_cancelToken.IsCancellationRequested )
+        _cancelToken.Cancel();
+    }
+
+    public IDisposable Subscribe( IObserver<T> observer )
+    {
+      return _subject.Value.Subscribe( observer );
+    }
+  }
+}
---
+++ Source/ReactiveTests/Tests/SingletonObservableTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ReactiveTests.Tests
+{
+  public class SingletonObservableTests
+  {
+    [Test]
+    public void SingletonObservableDoesNotify()
+    {
+      var observable = new SingletonObservable<DateTime>( (observer) =>
+      {
+        bool isAlive = true;
+
+        Task.Run( async () =>
+          {
+            while (isAlive)
+            {
+              observer.OnNext( DateTime.Now );
+              await Task.Delay( 100 );
+            }
+          } );
+
+        return Disposable.Create( () => isAlive = false );
+      } );
+    }
+  }
+}
