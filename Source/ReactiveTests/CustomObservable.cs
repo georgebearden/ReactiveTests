d808f42feb8ca1c9ad89ef73b294e97ab911a1b3
George Bearden <george@goerden.com>
2015-12-03 05:43:03 -0800

    Work on derived ObservableBase.

---
+++ Source/ReactiveTests/CustomObservable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReactiveTests
+{
+  public class CustomObservable : ObservableBase<DateTime>
+  {
+    protected override IDisposable SubscribeCore(IObserver<DateTime> observer)
+    {
+      return Observable.Create<DateTime>(async observer =>
+      {
+        var isRunning = true;
+
+        while (isRunning)
+        {
+          observer.OnNext(DateTime.Now);
+          await Task.Delay(100);
+        }
+
+        return Disposable.Create(() => isRunning = false);
+      });
+    }
+  }
+}
